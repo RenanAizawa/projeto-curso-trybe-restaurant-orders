a4bd908f07d6c0a8e1e5d8464dd4e454576d8c55
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-03-22 15:49:52 -0400

    [fix]: retirada de linhas em branco no arquivo src/analyze_log.py

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -4,10 +4,10 @@ import csv
 def analyze_log(path_to_file):
     if not path_to_file.endswith('.csv'):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
-        
+
     try:
         with open(path_to_file) as file:
             logs = csv.reader(file)
             print(logs)
     except FileNotFoundError:
-        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")    
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
