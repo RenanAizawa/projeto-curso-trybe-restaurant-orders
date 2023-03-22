aa5675e9f0fc1f51b2ec68e9b23869e752f1412b
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-03-22 15:43:20 -0400

    [feat]: desenvolvido retorno das falhas de leitura do arquivo

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,13 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-    # iniciando o projeto S2
+    if not path_to_file.endswith('.csv'):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+        
+    try:
+        with open(path_to_file) as file:
+            logs = csv.reader(file)
+            print(logs)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")    
